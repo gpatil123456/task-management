2f3fc3be68fc509e4e91bad7eae09154e7c0608a
Gaurav Pralhad Patil <100192960+gpatil123456@users.noreply.github.com>
2024-05-31 09:13:24 +0530

    change for calendar

--- praci.py
+++ praci.py
@@ -11,73 +11,51 @@ dftask = get_task_list()
 # Rename column names
 dftask = dftask.rename(columns={'Task': 'groupId', 
                                 'Task_Details': 'title',
-                                'Id':'id',
-                                'Start Date':'start',
-                                'Due Date':'end',
-                                'Owner':'resourcesid'
+                                'Id': 'id',
+                                'Start Date': 'start',
+                                'Due Date': 'end',
+                                'Owner': 'resourceId'
                                 })
 
 # Format 'start' and 'end' columns
 dftask['start'] = dftask['start'].str.replace('_', 'T')
 dftask['end'] = dftask['end'].str.replace('_', 'T')
 
-# Extract resources
-owner_lists = []
-for owner_string in dftask['resourcesid']:
-    owner_string = owner_string.strip("[]")
-    emails = [email.strip().strip('"') for email in owner_string.split(",")]
-    owner_lists.extend(emails)
+# Set up calendar resources
+calendar_resources = dftask[['groupId', 'resourceId']].drop_duplicates()
 
 # Create events from task data
-
-# Set up calendar resources
-# # Set up calendar resources
-# calendar_resources = [{"id": resource, "title": title} for resource, title in zip(owner_lists, dftask['title'])]
-# calendar_resources = [{"id": resource, "title": resource} for resource in owner_lists]
-calendar_resources=dftask.resourcesid.unique()
-
-# id = pd.DataFrame(calendar_resources, columns=['id'])
-# id['title'] = id['id']
-# id = id.reset_index(drop=True)
-# my_records = id.to_dict(orient='records')
-
-# calendar_events = []
-# for index row in id.iterrows():
-#     calendar_event = {
-#         "id": row['id'],
-#         "title": row['title']
-#     }
-  
-#     calendar_events.append(calendar_event)
-# st.write(calendar_events)
-# print(calendar_events)
-id = pd.DataFrame(calendar_resources, columns =['id'])
-id['title'] = id['id']
-id['Owner']=id['id']
-id = id.reset_index(drop=True)
-my_records =  id.to_dict(orient='records')
-
-# my_records=id.T.to_dict('records')
-calendar_events=[]
-for index, row in id.iterrows():
-    calendar_event = {
+events = []
+for index, row in dftask.iterrows():
+    event = {
+        "title": row['title'],
+        "start": row['start'],
+        "end": row['end'],
         "id": row['id'],
-        "Owner":row['Owner'],
-        "title":row['title'],      
+        "resourceId": row['resourceId']
     }
-    calendar_events.append(calendar_event)
-st.write(calendar_events)
+    events.append(event)
 
+# Create calendar resources from task data
+calendar_resources_list = []
+for _, resource in calendar_resources.iterrows():
+    calendar_resource = {
+        "id": resource['resourceId'],       # Resource ID
+        "title": resource['resourceId'],    # Resource title
+    }
+    calendar_resources_list.append(calendar_resource)
+
+# Set up calendar options
 calendar_options = {
-    "editable": "true",
-    "navLinks": "true",
-    "resources": calendar_events,
-    "selectable": "true",
+    "editable": True,
+    "navLinks": True,
+    "resources": calendar_resources_list,
+    "selectable": True,
     "initialDate": "2023-07-01",
     "initialView": "dayGridMonth",
+    "resourceAreaHeaderContent": "Resource"
 }
 
-# Set up calendar mode
 mode = st.selectbox(
     "Calendar Mode:",
     (
@@ -91,70 +69,88 @@ mode = st.selectbox(
         "multimonth",
     ),
 )
-events = []
-for index, row in dftask.iterrows():
-    event = {
-        "title": row['title'],
-        "start": row['start'],
-        "end": row['end'],
-        "groupId": row['groupId'],  
-        "resourcesid": row['resourcesid'] 
-    }
-    events.append(event)
-st.write(events)
+
 if "resource" in mode:
-    calendar_options["resourceGroupField"] = "title"
+    if mode == "resource-daygrid":
+        calendar_options.update({
+            "initialDate": "2023-07-01",
+            "initialView": "resourceDayGridDay",
+        })
+    elif mode == "resource-timeline":
+        calendar_options.update({
+            "headerToolbar": {
+                "left": "today prev,next",
+                "center": "title",
+                "right": "resourceTimelineDay,resourceTimelineWeek,resourceTimelineMonth",
+            },
+            "initialDate": "2023-07-01",
+            "initialView": "resourceTimelineDay",
+        })
+    elif mode == "resource-timegrid":
+        calendar_options.update({
+            "initialDate": "2023-07-01",
+            "initialView": "resourceTimeGridDay",
+        })
+else:
+    if mode == "daygrid":
+        calendar_options.update({
+            "headerToolbar": {
+                "left": "today prev,next",
+                "center": "title",
+                "right": "dayGridDay,dayGridWeek,dayGridMonth",
+            },
+            "initialDate": "2023-07-01",
+            "initialView": "dayGridMonth",
+        })
+    elif mode == "timegrid":
+        calendar_options.update({
+            "initialView": "timeGridWeek",
+        })
+    elif mode == "timeline":
+        calendar_options.update({
+            "headerToolbar": {
+                "left": "today prev,next",
+                "center": "title",
+                "right": "timelineDay,timelineWeek,timelineMonth",
+            },
+            "initialDate": "2023-07-01",
+            "initialView": "timelineMonth",
+        })
+    elif mode == "list":
+        calendar_options.update({
+            "initialDate": "2023-07-01",
+            "initialView": "listMonth",
+        })
+    elif mode == "multimonth":
+        calendar_options.update({
+            "initialView": "multiMonthYear",
+        })
 
-if mode == "daygrid":
-    calendar_options["headerToolbar"] = {
-        "left": "today prev,next",
-        "center": "title",
-        "right": "dayGridDay,dayGridWeek,dayGridMonth",
+# Render the calendar
+state = calendar(
+    events=st.session_state.get("events", events),
+    options=calendar_options,
+    custom_css="""
+    .fc-event-past {
+        opacity: 0.8;
     }
-elif mode == "timegrid":
-    calendar_options["initialView"] = "timeGridWeek"
-elif mode == "timeline":
-    calendar_options["headerToolbar"] = {
-        "left": "today prev,next",
-        "center": "title",
-        "right": "timelineDay,timelineWeek,timelineMonth",
+    .fc-event-time {
+        font-style: italic;
     }
-    calendar_options["initialView"] = "timelineMonth"
-elif mode == "list":
-    calendar_options["initialView"] = "listMonth"
-elif mode == "multimonth":
-    calendar_options["initialView"] = "multiMonthYear"
+    .fc-event-title {
+        font-weight: 700;
+    }
+    .fc-toolbar-title {
+        font-size: 2rem;
+    }
+    """,
+    key=mode,
+)
 
-# Set up Streamlit columns
-col1, col2 = st.columns([8, 2])
+# Update session state with new events if modified
+if state.get("eventsSet") is not None:
+    st.session_state["events"] = state["eventsSet"]
 
-with col1:
-    state = calendar(
-        events=st.session_state.get("events", events),
-        options=calendar_options,
-        custom_css="""
-        .fc-event-past {
-            opacity: 0.8;
-        }
-        .fc-event-time {
-            font-style: italic;
-        }
-        .fc-event-title {
-            font-weight: 700;
-        }
-        .fc-toolbar-title {
-            font-size: 2rem;
-        }
-        """,
-        key=mode,
-    )
-
-    if state.get("eventsSet") is not None:
-        st.session_state["events"] = state["eventsSet"]
-
-with col2:
-    if state.get("eventClick") is not None:
-        event_info = state["eventClick"].get("event")
-        if event_info:
-            st.write("Clicked Event:", event_info)
-            st.write("Event Resources:", event_info.get("resources"))
+# Display API reference
+# st.markdown("## API reference")
+# st.help(calendar)
